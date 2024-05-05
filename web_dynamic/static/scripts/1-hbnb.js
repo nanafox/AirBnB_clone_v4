0f233750d485f210656367a6356c7392b327b13c
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-05-05 20:38:59 +0000

    refactor: Fix semistandard errors

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
-  const amenitiesHeading4 = $("div.amenities h4");
-  const amenities = $("div.amenities .popover ul input:checkbox");
+  const amenitiesHeading4 = $('div.amenities h4');
+  const amenities = $('div.amenities .popover ul input:checkbox');
   const amenitiesDict = {};
 
-  amenities.on("click", function () {
-    const dataId = $(this).attr("data-id");
+  amenities.on('click', function () {
+    const dataId = $(this).attr('data-id');
 
-    if ($(this).is(":checked")) {
-      const dataName = $(this).attr("data-name");
+    if ($(this).is(':checked')) {
+      const dataName = $(this).attr('data-name');
 
       amenitiesDict[dataId.toString()] = dataName;
     } else {
       delete amenitiesDict[dataId];
     }
 
-    amenitiesHeading4.text(Object.values(amenitiesDict).join(", "));
+    amenitiesHeading4.text(Object.values(amenitiesDict).join(', '));
   });
 });
